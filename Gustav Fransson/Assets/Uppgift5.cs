c45f1c632df9cb0f3795ec2958ecbee8ad2b16ec
Gustav Entertainment <gurra001@live.se>
2017-03-01 08:34:13 +0100

    Sup y'all

--- Gustav Fransson/Assets/Uppgift5.cs
+++ Gustav Fransson/Assets/Uppgift5.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Uppgift5 : MonoBehaviour {
-public float a;
-public float b;
-public float c;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class Uppgift5 : MonoBehaviour
+{
+    public int a = 10;
+    public int b = 5;
+    private int c;
+    public float d = 10;
+    public float e = 0;
+
+    int Method(int a, int b)
+    {
+        return a + b;
+        //Its a method
+    }
+    // Use this for initialization
+    void Start()
+    {
+        c = Method(a, b);
+        if (c < e && c > d)
+        {
+            Debug.Log("Cisco");
+        }
+        //Prints if c is bigger than d and smaller than e.
+        else
+        {
+            Debug.Log("Cisco");
+        }
+        //Prints if c is biger than e or smaller than d.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
 }
